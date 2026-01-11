77729d022695eeea20d8b72e5aeed5c2844009eb
Kuda Developer <shang@kuda.dev>
2026-01-11 21:34:06 +1100

    chore: add .gitignore and exclude build directory

--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shang/Code/kuda/tests
-# Build directory: /home/shang/Code/kuda/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
